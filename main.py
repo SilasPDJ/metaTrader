4090c7fef3de0be6da1d255f60bb90c580475f1c
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-07-13 15:13:55 -0300

    init main

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import MetaTrader5 as mt5
+from datetime import datetime
+dia = datetime(2023, 7, 12)
+
+
+mt5.initialize()
+mt5.initialize()
+d = mt5.terminal_info()
+d.community_connection
+
+dados = mt5.copy_ticks_from('PETR4', dia, 10, mt5.COPY_TICKS_ALL)
+df = pd.DataFrame(dados)
+print(s)
