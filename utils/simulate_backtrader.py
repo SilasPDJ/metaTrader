800db4d295c48da3b85d67dde88c3f7f8c32ef92
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-07-17 21:09:15 -0300

    ...

--- utils/simulate_backtrader.py
+++ utils/simulate_backtrader.py
@@ -15,43 +15,29 @@ class MyStrategy(bt.Strategy):
         self.stop_loss = None
         self.take_profit = None
 
-    def notify_order(self, order):
-        if order.status in [order.Submitted, order.Accepted]:
-            return
-
-        if order.status in [order.Completed]:
-            if order.isbuy():
-                self.price = order.executed.price
-                self.stop_loss = self.price - self.p.quantidade_pontos
-                self.take_profit = self.price + self.p.quantidade_pontos
-                print(f"Buy Order executed: Price: {self.price:.2f}, Stop Loss: {self.stop_loss:.2f}, Take Profit: {self.take_profit:.2f}")
-            elif order.issell():
-                self.price = order.executed.price
-                self.stop_loss = self.price + self.p.quantidade_pontos
-                self.take_profit = self.price - self.p.quantidade_pontos
-                print(f"Sell Order executed: Price: {self.price:.2f}, Stop Loss: {self.stop_loss:.2f}, Take Profit: {self.take_profit:.2f}")
-
-
     def next(self):
         if self.order:
             return
+
         # Verifica se a distância de abertura para fechamento é igual a quantidade_pontos para operações de compra
         distance_buy = self.dataclose[0] - self.dataopen[0]
-        # print(f"Buy Distance: {distance_buy:.2f}, Quantity of Points: {self.p.quantidade_pontos:.2f}")
 
         if distance_buy*-1 >= self.p.quantidade_pontos:
-            print("Buy Condition met. Placing orders.")
             # Adiciona stop loss e take profit
+            self.price = self.dataclose[0]
+            self.stop_loss = self.price - self.p.quantidade_pontos
+            self.take_profit = self.price + self.p.quantidade_pontos
             self.buy(exectype=bt.Order.Stop, price=self.stop_loss)
             self.buy(exectype=bt.Order.Limit, price=self.take_profit)
 
         # Verifica se a distância de abertura para fechamento é igual a quantidade_pontos para operações de venda
         distance_sell = self.dataopen[0] - self.dataclose[0]
-        # print(f"Sell Distance: {distance_sell:.2f}, Quantity of Points: {self.p.quantidade_pontos:.2f}")
 
         if distance_sell*-1 >= self.p.quantidade_pontos:
-            print("Sell Condition met. Placing orders.")
             # Adiciona stop loss e take profit
+            self.price = self.dataclose[0]
+            self.stop_loss = self.price + self.p.quantidade_pontos
+            self.take_profit = self.price - self.p.quantidade_pontos
             self.sell(exectype=bt.Order.Stop, price=self.stop_loss)
             self.sell(exectype=bt.Order.Limit, price=self.take_profit)
 
@@ -60,6 +46,10 @@ if __name__ == "__main__":
 
     # Load your historical data into a pandas DataFrame
     data = pd.read_csv('historical_data.csv', index_col='time', parse_dates=True)
+
+    # Verifica se os dados estão em ordem crescente, caso contrário, inverte a ordem
+    if data.index[0] > data.index[1]:
+        data = data.iloc[::-1]
 
     # Convert the DataFrame to a 'backtrader' data feed
     data_feed = bt.feeds.PandasData(dataname=data)
