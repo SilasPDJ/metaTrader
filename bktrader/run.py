56bcead6baf9738af50b6e84352866e72e00b0f6
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-07-23 01:14:04 -0300

    goldencross?

--- bktrader/run.py
+++ bktrader/run.py
@@ -3,19 +3,6 @@ import pandas as pd
 import backtrader as bt
 from strategies.GoldenCross import GoldenCross
 from strategies.BuyHold import BuyHold
-strategies = {
-    "golden_cross": GoldenCross,
-    "buy_hold": BuyHold
-}
-
-parser = argparse.ArgumentParser()
-parser.add_argument("strategy", help="which stratetigy to run", type=str)
-args = parser.parse_args()
-
-if not args.strategy in strategies:
-    print(f"Invalid Strategy must be one of {strategies.keys()}")
-    sys.exit()
-
 
 cerebro = bt.Cerebro()
 cerebro.broker.set_cash(100000)
@@ -25,6 +12,6 @@ spy_prices = pd.read_csv('data/SPY.csv', index_col='Date', parse_dates=True)
 feed = bt.feeds.PandasData(dataname=spy_prices)
 cerebro.adddata(feed)
 
-cerebro.addstrategy(strategies[args.strategy])
+cerebro.addstrategy(GoldenCross)
 cerebro.run()
 cerebro.plot()
